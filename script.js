56475f90c3b38207e3ca40b1f21fb1f3a2094c01
Aniket Samanta <105125338+aniksam-github@users.noreply.github.com>
2023-03-23 11:05:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ class Calculator {
         case '*':
           computation = prev * current
         break
-        case '÷':
+        case '/':
         computation = prev / current
         break
         default:
